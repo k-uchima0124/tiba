fc915d5ae1b8e3511ff5f2e3d4ccaf58991b68e1
u_kuuto <u_kuuto@LAPTOP-U8SCROP6>
2025-05-14 12:35:12 +0900

    ｓｄｆｇｈ

--- src/dao/TeacherDao.java
+++ src/dao/TeacherDao.java
@@ -1,11 +1,7 @@
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import bean.TEACHER;
 
 public class TeacherDao extends Dao {
     public TEACHER get(String username, String password) {
-
+    	
     }
 }
