1831a7aa3e3013de42a0b37c255c80a3bd109a77
u_kuuto <u_kuuto@LAPTOP-U8SCROP6>
2025-05-14 12:39:45 +0900

    wooooooooooooooooooooaaaaaaaaaaaaaaa

--- src/dao/TeacherDao.java
+++ src/dao/TeacherDao.java
@@ -1,10 +1,11 @@
+package dao;
 import bean.TEACHER;
 
-public class TeacherDao extends Dao {
+public class TeacherDao {
     public TEACHER get(String username, String password) {
 
     	TEACHER teacher = null;
 
-
+    	return teacher;
     }
 }
