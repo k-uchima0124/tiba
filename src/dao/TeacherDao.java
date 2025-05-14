c95c0a025d9d4961984b0c125484496b75a42812
u_kuuto <u_kuuto@LAPTOP-U8SCROP6>
2025-05-14 12:34:53 +0900

    teacerhdao作ったお＾＾

---
+++ src/dao/TeacherDao.java
@@ -0,0 +1,11 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import bean.TEACHER;
+
+public class TeacherDao extends Dao {
+    public TEACHER get(String username, String password) {
+
+    }
+}
