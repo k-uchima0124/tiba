58e7b346b93f370440422609c51883b516e95dd3
K-Takahashiiii <fko2447008@stu.o-hara.ac.jp>
2025-05-20 12:28:31 +0900

    作成途中

--- src/scoremanager/LoginExecuteAction.java
+++ src/scoremanager/LoginExecuteAction.java
@@ -1,8 +1,32 @@
 package scoremanager;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Teacher;
+import dao.TeacherDao;
 import tool.Action;
+
 
 public class LoginExecuteAction extends Action{
 
+	public String execute(
+		HttpServletRequest request, HttpServletResponse response
+	) throws Exception {
+
+		HttpSession session = request.getSession();
+
+		String id = request.getParameter("id");
+		String password = request.getParameter("pass");
+
+		TeacherDao dao = new TeacherDao();
+
+		Teacher teacher = dao.login(id, password);
 
 
+
+
+
+		return "menu.jsp";
+	}
 }
